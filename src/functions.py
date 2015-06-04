6c9ce8d7face517277c9b4327c37b807c47eb1fd
lbarrios <lbarrios@dc.uba.ar>
2015-06-04 15:52:48 -0300

    Arreglado check_sudo para evitar incompatibilidad con mac

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,12 @@
 # check sudo
 def check_sudo():
-    import os
-    if os.getuid() != 0:
-        raise RuntimeError("You need to run this script with sudo!")      
+    from sys import platform as _platform
+    if _platform == "linux" or _platform == "linux2":
+        # Linux
+        import os
+        if os.getuid() != 0:
+            raise RuntimeError("\n\nYou need to run this script with sudo!")
+    #elif _platform == "darwin":
+        # MAC OS X
+    #elif _platform == "win32":
+        # Windows
