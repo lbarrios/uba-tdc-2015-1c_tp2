9d32413837706a28605ce692353a0398bfe2b263
gbenegas <gsbenegas@gmail.com>
2015-06-14 16:59:21 -0300

    fix bug

--- src/run_all.py
+++ src/run_all.py
@@ -7,6 +7,6 @@ os.system('python traceroute.py ' + url)
 functions.plot_routes(url + '.json', url + '.html')
 
 cmd = 'xdg-open %s.html' % url
-if _platform == 'darwin':
+if sys.platform == 'darwin':
     cmd = 'open %s.html' % url
 os.system(cmd)
