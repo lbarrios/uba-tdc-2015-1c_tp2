c784b8407c9b03e9f63f60d2acaf6a17a1279705
gbenegas <gsbenegas@gmail.com>
2015-06-15 12:09:29 -0300

    mejorado el graficador de rtt

--- src/rtt_barplot.py
+++ src/rtt_barplot.py
@@ -8,20 +8,17 @@ output_file = sys.argv[2]
 with open(input_file) as f:
     hops = json.loads(f.read())
     
-rtt_acumulado = [(np.average([int(med["rtt"][:-3]) if med["rtt"]!='*' else np.nan
+rtt_acumulado = [(np.average([int(med["rtt"][:-3])
+                              if med["rtt"]!='*' else np.nan
                           for med in hop]))
                  for hop in hops]
-print rtt_acumulado
-
-# rtt = []
-# for i in range(len(rtt_acumulado)):
-#     if i == 0:
-#         hop_rtt = rtt_acumulado[0]
-#     else:
-#         hop_rtt = rtt_acumulado[i] - rtt_acumulado[i-1]
-#     rtt.append(hop_rtt)
-# print rtt
 
 hop_number = range(1, len(rtt_acumulado) + 1)
-plt.bar(hop_number, rtt_acumulado)
+ind = np.arange(len(hop_number))
+width = 1.0
+
+plt.bar(ind, rtt_acumulado, width)
+plt.xticks(ind + width / 2.0, hop_number)
+plt.xlabel('Hop number')
+plt.ylabel('Round trip time(ms)')
 plt.savefig(output_file)
